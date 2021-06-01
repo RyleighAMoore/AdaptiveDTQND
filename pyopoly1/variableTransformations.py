05afbbafc53a54e21482febfb5cf9f5e5117c6d4
rmoore <rmoore@math.utah.edu>
2021-06-01 12:37:42 -0600

    updated VariableTransformation for 1D

--- pyopoly1/variableTransformations.py
+++ pyopoly1/variableTransformations.py
@@ -2,6 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def map_to_canonical_space(user_samples, scale_parameters):
+    dimension = np.size(user_samples,1)
+    if dimension ==1:
+        Linv = 1/np.sqrt(scale_parameters.cov)
+        mean = scale_parameters.mu
+        delta = np.zeros(np.shape(user_samples))
+        shiftedMesh = user_samples - delta
+        canonical_samples = (Linv*shiftedMesh.T).T
+        return canonical_samples
+    
     L = np.linalg.cholesky((scale_parameters.cov))
     Linv = np.linalg.inv(L) 
     mean = scale_parameters.mu
@@ -18,6 +27,15 @@ def map_to_canonical_space(user_samples, scale_parameters):
     return canonical_samples
 
 def map_from_canonical_space(user_samples, scale_parameters):
+    dimension = np.size(user_samples,1)
+    if dimension ==1:
+        L = np.sqrt(scale_parameters.cov)
+        mean = scale_parameters.mu
+        delta = np.zeros(np.shape(user_samples))
+        vals = L*np.asarray(user_samples).T + delta.T
+        return vals.T
+        
+    
     mean = scale_parameters.mu
     delta = np.zeros(np.shape(user_samples))
     for i in range(np.size(mean)):
