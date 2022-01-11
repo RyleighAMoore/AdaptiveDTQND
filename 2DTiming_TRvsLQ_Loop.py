c715f891c9d5840366402d75fe7514bec3a8a10f
rmoore <rmoore@math.utah.edu>
2022-01-11 11:34:10 -0700

    Going to clean up classes a little ~

--- 2DTiming_TRvsLQ_Loop.py
+++ 2DTiming_TRvsLQ_Loop.py
@@ -12,11 +12,11 @@ import time
 dimension = 2
 radius = 2
 h = 0.05
-betaVals = [3]
+betaVals = [2.5, 3]
 bufferVals = [0, 0.5]
 endTime = 20 #30
 spacingLQVals = [0.38]
-spacingTRVals = [0.25, 0.2]
+spacingTRVals = [0.25, 0.2, 0.18]
 
 
 # SDE creation
