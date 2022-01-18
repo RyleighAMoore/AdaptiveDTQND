3432a70ba182f2018d77503d3a5b2b36094b98ef
rmoore <rmoore@math.utah.edu>
2022-01-17 21:30:06 -0700

    Changes for server run

--- 2DTiming_TRvsLQ_Loop.py
+++ 2DTiming_TRvsLQ_Loop.py
@@ -12,11 +12,21 @@ import time
 dimension = 2
 radius = 2
 h = 0.05
-betaVals = [2.5]
-bufferVals = [0]
-endTime = 20
+betaVals = [3]
+bufferVals = [0, 0.3, 0.5]
+endTime = 25
 spacingLQVals = [0.38]
-spacingTRVals = []#[0.25, 0.2]
+spacingTRVals = [0.25, 0.2]
+
+
+# dimension = 2
+# radius = 2
+# h = 0.05
+# betaVals = [3]
+# bufferVals = [0, 0.3, 0.5]
+# endTime = 20
+# spacingLQVals = [0.38]
+# spacingTRVals = [0.2]
 
 
 # SDE creation
