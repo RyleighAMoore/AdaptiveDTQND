724cd270c2ec1a1b1c848560807055b4c10004fd
rmoore <rmoore@math.utah.edu>
2022-02-23 14:43:38 -0700

    2D simulation

--- EXAMPLE_2DSimulations.py
+++ EXAMPLE_2DSimulations.py
@@ -13,7 +13,7 @@ from Functions import get2DTrapezoidalMeshBasedOnLejaQuadratureSolutionMovingHil
 from Errors import ErrorValsOneTime
 
 
-problem = "erf" # "spiral" "complex" "hill"
+problem = "complex" # "spiral" "complex" "hill"
 approxError = False
 dimension = 2
 timeDiscretizationType = "EM"
