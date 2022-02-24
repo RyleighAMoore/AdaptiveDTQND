debd7d11b37745ed696a84b6edbc633e39a57e18
rmoore <rmoore@math.utah.edu>
2022-02-24 00:18:35 -0700

    2D simulation

--- EXAMPLE_2DSimulations.py
+++ EXAMPLE_2DSimulations.py
@@ -35,7 +35,7 @@ if problem == "erf":
     diffusionFunction = functionBank.pt75Diffusion
     spatialDiff = False
     kstepMin = 0.2
-    kstepMax = 0.22
+    kstepMax = 0.2
     endTime = 4
     radius = 3
     beta = 4
@@ -133,7 +133,7 @@ if problem == "hill":
     plotRowNinePlots(plottingMax, meshTrajectoryLQ, pdfLQ, meshTrajectoryTR, pdfTR, h, [5, 15,-1], [-12,12,-12,12], simulation.times)
 
 if problem == "erf":
-    plotRowNinePlots(plottingMax, meshTrajectoryLQ, pdfLQ, meshTrajectoryTR, pdfTR, h, [3, 29,-1], [-12,12,-12,12], simulation.times)
+    plotRowNinePlots(plottingMax, meshTrajectoryLQ, pdfLQ, meshTrajectoryTR, pdfTR, h, [3, 15,-1], [-12,12,-12,12], simulation.times)
 
 if problem == "spiral":
     plotRowNinePlots(plottingMax,meshTrajectoryLQ, pdfLQ, meshTrajectoryTR, pdfTR, h, [19, 69 ,-1],[-10,10,-10,10], simulation.times)
